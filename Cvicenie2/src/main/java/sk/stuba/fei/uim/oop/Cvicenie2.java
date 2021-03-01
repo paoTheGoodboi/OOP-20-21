c769c6a6c3cc73b4aeca730ffe80bf2205201a0e
paoTheGoodboi <xlukacp1@stuba.sk>
2021-03-01 12:27:23 +0100

    Porovnanie objektov

--- Cvicenie2/src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
+++ Cvicenie2/src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
@@ -1,8 +1,20 @@
 package sk.stuba.fei.uim.oop;
 
+import java.sql.SQLOutput;
+import java.util.Objects;
+
 public class Cvicenie2 {
     public static void main(String[] args){
-        int x = KeyboardInput.readInt();
-        System.out.println(x);
+        String a = "a";
+        String b = null;
+        String c = KeyboardInput.readString("zadajte text");
+        System.out.println(a == a);
+        System.out.println(a == b);
+        System.out.println(a == c);
+        System.out.println(a.equals(c));
+        System.out.println(Objects.equals(a, c));
+        System.out.println(Objects.equals(b, c));
+//        System.out.println();
+
     }
 }
