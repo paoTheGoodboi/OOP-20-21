4a11804f6186ad83e3031068145d3cba64556e36
paoTheGoodboi <xlukacp1@stuba.sk>
2021-03-01 12:06:34 +0100

    Zklavesnice

---
+++ Cvicenie2/src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
@@ -0,0 +1,8 @@
+package sk.stuba.fei.uim.oop;
+
+public class Cvicenie2 {
+    public static void main(String[] args){
+        int x = KeyboardInput.readInt();
+        System.out.println(x);
+    }
+}
