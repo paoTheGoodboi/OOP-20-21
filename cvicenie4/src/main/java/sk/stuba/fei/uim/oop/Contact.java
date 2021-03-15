9a929b13746e76b6b0f8d5ca8ac4b7b9f1306ff1
paoTheGoodboi <xlukacp1@stuba.sk>
2021-03-15 11:53:34 +0100

    var variables

---
+++ cvicenie4/src/main/java/sk/stuba/fei/uim/oop/Contact.java
@@ -0,0 +1,12 @@
+package sk.stuba.fei.uim.oop;
+
+public class Contact {
+
+    String firstName;
+    String surName;
+
+    public Contact(String firstName, String surName) {
+        this.firstName = firstName;
+        this.surName = surName;
+    }
+}
---
+++ cvicenie4/src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -0,0 +1,11 @@
+package sk.stuba.fei.uim.oop;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Contact contact = new Contact("Pavol","Lukac");
+        var contact2 = new Contact("Peter","Lukac");
+        var cislo = 2;
+    }
+
+}
