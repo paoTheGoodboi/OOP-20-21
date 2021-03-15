dc7c10b81a0c651d7ffc1b0db9b640b1528dc4f6
paoTheGoodboi <xlukacp1@stuba.sk>
2021-03-15 12:13:46 +0100

    object references

--- cvicenie4/src/main/java/sk/stuba/fei/uim/oop/Contact.java
+++ cvicenie4/src/main/java/sk/stuba/fei/uim/oop/Contact.java
@@ -2,11 +2,35 @@ package sk.stuba.fei.uim.oop;
 
 public class Contact {
 
-    String firstName;
-    String surName;
+    private String firstName;
+    private String surname;
+
+    private Contact[] friends;
 
     public Contact(String firstName, String surName) {
         this.firstName = firstName;
-        this.surName = surName;
+        this.surname = surName;
+    }
+
+    public Contact(String fullName) {
+        var parts = fullName.split(" ");
+        firstName = parts[0];
+        surname = parts[1];
+    }
+
+    public static Contact parseFromFullName(String fullName) {
+        return new Contact(fullName);
+    }
+
+    public void setFriends(Contact... newFriends) {
+        this.friends = newFriends;
+    }
+
+    public void setFriendsFromArray(Contact[] newFriends) {
+        this.friends = newFriends;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 }
--- cvicenie4/src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ cvicenie4/src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -5,7 +5,20 @@ public class Main {
     public static void main(String[] args) {
         Contact contact = new Contact("Pavol","Lukac");
         var contact2 = new Contact("Peter","Lukac");
-        var cislo = 2;
+        var contac3 = new Contact("Pavol Lukac");
+
+        var pat = Integer.parseInt("5");
+        var contact4 = Contact.parseFromFullName("Pavol Lukac");
+
+        var contacts1 = new Contact[] {contact2, contac3, contact4};
+        contact.setFriendsFromArray(contacts1);
+
+        contacts1[0].setFirstName("Mato");
+
+        contact.setFriends(contact2,contac3,contact4);
+
+        contac3.setFirstName("Kovy");
     }
+
 
 }
