9024e9d9347f19205cae86404d135883326963e3
paoTheGoodboi <xlukacp1@stuba.sk>
2021-04-11 22:06:58 +0200

    ram graphics

--- Swing/src/main/java/sk/stuba/oop/MyCanvas.java
+++ Swing/src/main/java/sk/stuba/oop/MyCanvas.java
@@ -1,21 +1,26 @@
 package sk.stuba.oop;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class MyCanvas extends Canvas {
 
     @Override
     public void paint(Graphics g) {
-        g.setColor(Color.GRAY);
-        g.fillRect(100,150,100,100);
-        g.setColor(Color.RED);
-        g.fillPolygon(new int[] {100,200, 150}, new int[] {150, 150, 50}, 3);
+        var image = new BufferedImage(300,300, BufferedImage.TYPE_INT_BGR);
+        var ramGraphics = image.getGraphics();
 
-        g.setColor(Color.BLUE);
+        ramGraphics.setColor(Color.GRAY);
+        ramGraphics.fillRect(100,150,100,100);
+        ramGraphics.setColor(Color.RED);
+        ramGraphics.fillPolygon(new int[] {100,200, 150}, new int[] {150, 150, 50}, 3);
+
+        ramGraphics.setColor(Color.BLUE);
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                g.fillRect(110 + 45 * i, 160 + 45 * j, 35, 35);
+                ramGraphics.fillRect(110 + 45 * i, 160 + 45 * j, 35, 35);
             }
         }
+        g.drawImage(image,0,0,null);
     }
 }
